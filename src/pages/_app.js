b23fd6203e19fa109f80a92017745e54b99434e3
Drinor Berisha <drinorb66@gmail.com>
2024-01-29 14:25:50 +0100

    added speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,12 @@ import React from 'react';
 import '../../styles/globals.css';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
-
-
+import { SpeedInsights } from '@vercel/speed-insights/react';
 function MyApp({ Component, pageProps }) {
   return( 
   <Provider store={store}>
     <Component {...pageProps} />
+    <SpeedInsights/>
   </Provider>)
 }
 
