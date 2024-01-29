864f7481bdf8d40e3407fad7272fe8870f11b42a
Drinor Berisha <drinorb66@gmail.com>
2024-01-29 14:30:46 +0100

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,13 @@ import '../../styles/globals.css';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 function MyApp({ Component, pageProps }) {
   return( 
   <Provider store={store}>
     <Component {...pageProps} />
     <SpeedInsights/>
+    <Analytics/>
   </Provider>)
 }
 
